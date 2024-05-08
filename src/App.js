7931036412fad5cbdbd8bc68889700c83ff87180
prestonjcrowe <prestonjcrowe@gmail.com>
2024-05-07 22:12:05 -0700

    Basic edit kana menu implementation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,70 @@ import React, { useState, useEffect } from 'react';
 import { toRomaji } from 'wanakana';
 import './App.css';
 
+// function KanaOverlay() {
+//   return (
+
+//   )
+// }
+
+function KanaMenuOverlay({ isOpen, onClose, selected, toggleSelected }) {
+  const HIRAGANA_CHARACTERS = [
+    "あ", "い", "う", "え", "お",
+    "か","き", "く", "け", "こ",
+    "さ","し", "す", "せ", "そ",
+    "た","ち", "つ", "て", "と",
+    "な","に", "ぬ", "ね", "の",
+    "は","ひ", "ふ", "へ", "ほ",
+    "ま","み", "む", "め", "も",
+    "や", " ", "ゆ", " ", "よ",
+    "ら","り", "る", "れ", "ろ",
+    "わ"," ", " ", " ", "を",
+    " ", " ", "ん", " ", " ",
+    "が", "ぎ", "ぐ", "げ", "ご",
+    "ざ", "じ", "ず", "ぜ", "ぞ",
+    "だ", "ぢ", "づ", "で", "ど",
+    "ば", "び", "ぶ", "べ", "ぼ",
+    "ぱ", "ぴ", "ぷ", "ぺ", "ぽ",
+
+  ]
+
+  const KanaCharacter = ({ char }) => {
+    if (char === " ") {
+      return (<div></div>)
+    }
+
+
+    let className = selected.has(char) ? "kana-menu-character selected-kana" : "kana-menu-character";
+    console.log(className)
+    return (
+      <div className={className} onClick={() => toggleSelected(char)}>{char}</div>
+    )
+  }
+
+  if (!isOpen) {
+    return null;
+  }
+  
+  return (
+    <div className="kana-menu-container">
+      <div className="kana-menu-exit-button" onClick={onClose}>←</div>
+      <div className="kana-character-menu-container">
+        {HIRAGANA_CHARACTERS.map((c, i) => <KanaCharacter char={c} key={i}/>)}
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
 
   const [activeWordIndex, setActiveWordIndex] = useState(0);
-  const [selectedKana, setSelectedKana] = useState(new Set(["い", "う", "え", "お","か","き", "く", "け", "こ","さ","し", "す", "せ", "そ"]))
+  const [selectedKana, setSelectedKana] = useState(new Set([]))
   const [wordList, setWordList] = useState(new Set([]))
   const [showDetails, setShowDetails] = useState(false)
   const [dictionary, setDictionary] = useState([])
   const [entityCodes, setEntityCodes] = useState({})
+  const [kanaMenuOpen, setKanaMenuOpen] = useState(false)
 
   const shouldIncludeWord = (word) => {
     for (let i = 0; i < word.length; i++) {
@@ -21,7 +77,7 @@ function App() {
     return true
   }
 
-  const updateWordList = (selectedKana) => {
+  const updateWordList = () => {
     if (!dictionary || !("words" in dictionary)) {
       return;
     }
@@ -85,6 +141,20 @@ function App() {
   let activeDefinition = wordList.size > 0 ? [...wordList][activeWordIndex]["definition"] : "..."
   let activePartOfSpeech = wordList.size > 0 ? entityCodes[[...wordList][activeWordIndex]["partOfSpeech"]] : "..."
 
+  let toggleSelected = (char) => {
+    console.log(`Toggling ${char}`)
+    let newSelectedKana = new Set([...selectedKana])
+
+    if (selectedKana.has(char)) {
+      newSelectedKana.delete(char);
+    } else {
+      newSelectedKana.add(char);
+    }
+
+    console.log(newSelectedKana)
+    setSelectedKana(newSelectedKana);
+  }
+
 
   return (
     <div className="app-container">
@@ -102,9 +172,10 @@ function App() {
       </div>
       <div className="menu-container">
         <div className="menu-item">about</div>
-        <div className="menu-item">edit kana</div>
+        <div className="menu-item" onClick={() => setKanaMenuOpen(true)}>edit kana</div>
         <div className="menu-item">my stats</div>
       </div>
+      <KanaMenuOverlay selected={selectedKana} isOpen={kanaMenuOpen} onClose={() => setKanaMenuOpen(false)} toggleSelected={toggleSelected}/>
     </div>
   );
 }
