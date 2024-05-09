97879f0fa4b36fa3d9ae16d7b13c7a9443305d81
prestonjcrowe <prestonjcrowe@gmail.com>
2024-05-08 23:19:30 -0700

    Add katakana to edit kana overlay

--- src/App.js
+++ src/App.js
@@ -2,32 +2,60 @@ import React, { useState, useEffect } from 'react';
 import { toRomaji } from 'wanakana';
 import './App.css';
 
-// function KanaOverlay() {
-//   return (
-
-//   )
-// }
-
-function KanaMenuOverlay({ isOpen, onClose, selected, toggleSelected }) {
+const KanaMenuOverlay = ({ isOpen, onClose, selected, toggleSelected }) => {
+  const [alphabet, setAlphabet] = useState("hiragana");
   const HIRAGANA_CHARACTERS = [
     "あ", "い", "う", "え", "お",
-    "か","き", "く", "け", "こ",
-    "さ","し", "す", "せ", "そ",
-    "た","ち", "つ", "て", "と",
-    "な","に", "ぬ", "ね", "の",
-    "は","ひ", "ふ", "へ", "ほ",
-    "ま","み", "む", "め", "も",
+    "か", "き", "く", "け", "こ",
+    "さ", "し", "す", "せ", "そ",
+    "た", "ち", "つ", "て", "と",
+    "な", "に", "ぬ", "ね", "の",
+    "は", "ひ", "ふ", "へ", "ほ",
+    "ま", "み", "む", "め", "も",
     "や", " ", "ゆ", " ", "よ",
-    "ら","り", "る", "れ", "ろ",
-    "わ"," ", " ", " ", "を",
+    "ら", "り", "る", "れ", "ろ",
+    "わ", " ", " ", " ", "を",
     " ", " ", "ん", " ", " ",
     "が", "ぎ", "ぐ", "げ", "ご",
     "ざ", "じ", "ず", "ぜ", "ぞ",
     "だ", "ぢ", "づ", "で", "ど",
     "ば", "び", "ぶ", "べ", "ぼ",
     "ぱ", "ぴ", "ぷ", "ぺ", "ぽ",
-
   ]
+
+  const KATAKANA_CHARACTERS = [
+    "ア", "イ", "ウ", "エ", "オ",
+    "カ", "キ", "ク", "ケ", "コ",
+    "サ", "シ", "ス", "セ", "ソ",
+    "タ", "チ", "ツ", "テ", "ト",
+    "ナ", "ニ", "ヌ", "ネ", "ノ",
+    "ハ", "ヒ", "フ", "ヘ", "ホ",
+    "マ", "ミ", "ム", "メ", "モ",
+    "ヤ", " ", "ユ", " ", "ヨ",
+    "ラ", "リ", "ル", "レ", "ロ",
+    "ワ", " ", " ", " ", "ヲ",
+    " ", " ", "ン", " ", " ",
+    "ガ", "ギ", "グ", "ゲ", "ゴ",
+    "ザ", "ジ", "ズ", "ゼ", "ゾ",
+    "ダ", "ヂ", "ヅ", "デ", "ド",
+    "バ", "ビ", "ブ", "ベ", "ボ",
+    "パ", "ピ", "プ", "ペ", "ポ",
+  ]
+
+  const ALPHABETS = {
+    "hiragana": HIRAGANA_CHARACTERS,
+    "katakana": KATAKANA_CHARACTERS
+  }
+
+  const toggleAlphabet = () => {
+    setAlphabet((prev) => {
+      if (prev === "hiragana") {
+        return "katakana"
+      }
+
+      return "hiragana";
+    })
+  }
 
   const KanaCharacter = ({ char }) => {
     if (char === " ") {
@@ -45,12 +73,15 @@ function KanaMenuOverlay({ isOpen, onClose, selected, toggleSelected }) {
   if (!isOpen) {
     return null;
   }
-  
+
   return (
     <div className="kana-menu-container">
-      <div className="kana-menu-exit-button" onClick={onClose}>←</div>
+      <div className="kana-menu-controls-container">
+        <div className="kana-menu-exit-button" onClick={onClose}>←</div>
+        <div className="kana-menu-alphabet-button" onClick={toggleAlphabet}>{alphabet}</div>
+      </div>
       <div className="kana-character-menu-container">
-        {HIRAGANA_CHARACTERS.map((c, i) => <KanaCharacter char={c} key={i}/>)}
+        {ALPHABETS[alphabet].map((c, i) => <KanaCharacter char={c} key={i} />)}
       </div>
     </div>
   )
@@ -70,10 +101,10 @@ function App() {
   const shouldIncludeWord = (word) => {
     for (let i = 0; i < word.length; i++) {
       if (!selectedKana.has(word.charAt(i))) {
-          return false
+        return false
       }
     }
-  
+
     return true
   }
 
@@ -84,12 +115,12 @@ function App() {
 
     let newWordList = new Set([])
     for (const word of dictionary["words"]) {
-        let text = word["kana"][0]['text']
-        if (shouldIncludeWord(text)) {
-            let definition = word["sense"][0]["gloss"][0]["text"]
-            let partOfSpeech = word["sense"][0]["partOfSpeech"][0]
-            newWordList.add({"text": text, "definition": definition, "partOfSpeech": partOfSpeech})
-        }
+      let text = word["kana"][0]['text']
+      if (shouldIncludeWord(text)) {
+        let definition = word["sense"][0]["gloss"][0]["text"]
+        let partOfSpeech = word["sense"][0]["partOfSpeech"][0]
+        newWordList.add({ "text": text, "definition": definition, "partOfSpeech": partOfSpeech })
+      }
     }
 
     // console.log(`Selected Characters: ${selectedKana}`)
@@ -97,7 +128,7 @@ function App() {
     setWordList(newWordList)
     setActiveWordIndex(0)
   }
-  
+
   useEffect(() => {
     console.log("Dictionary or selected kana changed, updating wordList...")
     updateWordList(selectedKana)
@@ -116,30 +147,36 @@ function App() {
 
   useEffect(() => {
     const handleKeyUp = (e) => {
-      if (e.keyCode !== 32) {
+      if (e.keyCode !== 32 || wordList.size === 0) {
         return;
       }
 
       console.log(activeWordIndex, showDetails)
       if (showDetails === true) {
-        console.log('hello')
         setActiveWordIndex(prev => prev + 1)
       }
 
       setShowDetails(prev => !prev)
     }
-  
+
     window.document.addEventListener('keyup', handleKeyUp);
-  
+
     return () => {
       window.document.removeEventListener('keyup', handleKeyUp);
     }
-  }, [activeWordIndex, showDetails]);
+  }, [activeWordIndex, showDetails, wordList]);
+
+  const getRomajiString = (kana) => {
+    return `(${[...kana].map((c) => toRomaji(c)).join('·')})`
+  }
 
   let activeWord = wordList.size > 0 ? [...wordList][activeWordIndex]["text"] : "..."
-  let activeRomaji = wordList.size > 0 ? toRomaji(activeWord) : "..."
+  let activeRomaji = wordList.size > 0 ? getRomajiString(activeWord) : "..."
   let activeDefinition = wordList.size > 0 ? [...wordList][activeWordIndex]["definition"] : "..."
   let activePartOfSpeech = wordList.size > 0 ? entityCodes[[...wordList][activeWordIndex]["partOfSpeech"]] : "..."
+
+  // setting for words in order or random
+  // display word count (X out of Y remaining)
 
   let toggleSelected = (char) => {
     console.log(`Toggling ${char}`)
@@ -156,18 +193,28 @@ function App() {
   }
 
 
+
+
   return (
     <div className="app-container">
       <div className="banner-container">
-        <div className="banner-title">日本語.build</div>
-        <div className="banner-subtitle">kana word building tool</div>
-
+        <div className="banner-title">romaji.world</div>
+        <div className="banner-subtitle">(●´ω｀●)</div>
+        {/* <div className="banner-title">nihongo.build</div>
+        <div className="banner-subtitle">( • ̀ω•́ )✧</div> */}
+        {/* <div className="banner-title">日本語.build</div>
+        <div className="banner-subtitle">kana word building tool</div> */}
       </div>
       <div className="active-word-container">
-          <div id="active-word">{activeWord}</div>
-          <div id="active-word-romaji">{showDetails ? activeRomaji : ""}</div>
-          <div id="active-word-definition">{showDetails ? activeDefinition : ""}</div>
-          {/* <div id="active-word-pos">{showDetails ? activePartOfSpeech : ""}</div> */}
+        {
+          wordList.size > 0 ?
+            <div id="active-word">{activeWord}</div> :
+            <div id="starting-text">edit kana to begin</div>
+        }
+        <div id="active-word-romaji">{showDetails ? activeRomaji : ""}</div>
+        <div id="active-word-definition">{showDetails ? activeDefinition : ""}</div>
+
+        {/* <div id="active-word-pos">{showDetails ? activePartOfSpeech : ""}</div> */}
 
       </div>
       <div className="menu-container">
@@ -175,7 +222,7 @@ function App() {
         <div className="menu-item" onClick={() => setKanaMenuOpen(true)}>edit kana</div>
         <div className="menu-item">my stats</div>
       </div>
-      <KanaMenuOverlay selected={selectedKana} isOpen={kanaMenuOpen} onClose={() => setKanaMenuOpen(false)} toggleSelected={toggleSelected}/>
+      <KanaMenuOverlay selected={selectedKana} isOpen={kanaMenuOpen} onClose={() => setKanaMenuOpen(false)} toggleSelected={toggleSelected} />
     </div>
   );
 }
